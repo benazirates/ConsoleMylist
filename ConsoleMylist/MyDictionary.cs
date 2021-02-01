eda4181a1692d5c6e83714d89649998276e346e8
BENAZİR ATEŞ <benazirates@gmail.com>
2021-02-02 00:53:29 +0300

    Proje dosyası ekle.

---
+++ ConsoleMylist/MyDictionary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleMylist
+{
+    class MyDictionary<B>
+    {
+        B[] items;
+        
+        public MyDictionary()
+        {
+            items = new B[0];
+
+        }
+        public void Add(B item)
+        {
+            B[] tempArray = items;
+            items = new B[items.Length+1];
+            
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+                
+            }
+            items[items.Length - 1] = item;
+        }
+        public int Lenght
+        {
+            get { return items.Length; }
+        }
+        public B[] Items
+        {
+            get { return items; }
+        }
+    }
+}
---
+++ ConsoleMylist/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleMylist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> names = new  MyDictionary<string>();
+            names.Add("Matematik");
+            names.Add("Fizik");
+            names.Add("Kimya");
+            names.Add("Biyoloji");
+            Console.WriteLine(names.Lenght);
+            Console.WriteLine("**************************");
+
+            MyDictionary<int> times = new MyDictionary<int>();
+            times.Add(08);
+            times.Add(10);
+            times.Add(13);
+            times.Add(14);
+            Console.WriteLine(times.Lenght);
+            Console.WriteLine("**********************");
+
+            foreach (var item in names.Items)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("*********************");
+
+            foreach (var itemm in times.Items)
+            {
+                Console.WriteLine(itemm);
+            }
+
+        }
+    }
+}
